29d16c59e64f9e7a007ce9d9a7e604f2e91fc5d2
daveudith@gmail.com <daveudith@gmail.com>
2024-09-11 15:06:24 +0530

    create Dockerfile for containerize the api and create docker-compose.yml file for the mongoDB separate container

--- db.js
+++ db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://Udith:1234@atlascluster.jyup7.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
